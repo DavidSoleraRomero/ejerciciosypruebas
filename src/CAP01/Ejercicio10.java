f4ac5d2f30de183e080b64f74a9c24bfd8ab3368
DavidSoleraRomero <soleraromerodavid@gmail.com>
2023-10-05 08:31:03 +0200

    new file:   .vscode/settings.json 	modified:   bin/CAP01/Ejercicio10.class 	modified:   src/CAP01/Ejercicio10.java

--- src/CAP01/Ejercicio10.java
+++ src/CAP01/Ejercicio10.java
@@ -1,17 +1,22 @@
 package CAP01;
 
+import paqueteprueba.ConsoleColors;
+
 /* Mejora el ejercicio anterior añadiéndole colores. */
-// Los colores no funcionan en Windows por defecto. Podría funcionar con la librería JANSI
+// Los colores no funcionan en Windows por defecto.
 public class Ejercicio10 {
     public static void main(String[] args) {
         System.out.println("                                                             ");
-        System.out.println("                                         █████    █████      ");
-        System.out.println("                                      ████      ████         ");
-        System.out.println("                                   ████       ████           ");
-        System.out.println("                                  ███        ███             ");
-        System.out.println("        Aprendiendo Java         █████    ██████             ");
-        System.out.println("    con David Solera Romero     ██    ████    ██             ");
-        System.out.println("                                  ████    ████               ");
+        System.out.println("                                         " + ConsoleColors.RED + "█████    █████      ");
+        System.out.println("                                      " + ConsoleColors.BLUE + "████      ████         ");
+        System.out.println("                                   " + ConsoleColors.GREEN + "████       ████           ");
+        System.out.println(
+                "                                  " + ConsoleColors.PURPLE_BRIGHT + "███        ███             ");
+        System.out.println("        " + ConsoleColors.CYAN + "Aprendiendo Java" + ConsoleColors.YELLOW
+                + "         █████    ██████             ");
+        System.out.println("    " + ConsoleColors.CYAN_BOLD + "con David Solera Romero" + ConsoleColors.RED
+                + "     ██    ████    ██             ");
+        System.out.println("                                  " + ConsoleColors.YELLOW + "████    ████               ");
         System.out.println("                                                             ");
 
     }
