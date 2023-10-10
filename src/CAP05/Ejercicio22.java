cd2ce639d634a791e9fd4828096df975f2923ddb
DavidSoleraRomero <soleraromerodavid@gmail.com>
2023-10-10 09:02:03 +0200

    Ejercicio 22 - CAP05

--- src/CAP05/Ejercicio22.java
+++ src/CAP05/Ejercicio22.java
@@ -1,12 +1,25 @@
 package CAP05;
 
-import java.util.Scanner;
-
 /* Muestra por pantalla todos los números primos entre 2 y 100, ambos incluidos. */
 public class Ejercicio22 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        sc.close();
+        int num = 1;
+        System.out.println("<-- Lista de números primos entre el 2 y el 100 -->");
+        System.out.printf("---------------------------------------------------\n%d %d ", 2, 3);
+        while (true & num <= 100) {
+            int acum = 2;
+            while (true & acum <= (num / 2)) {
+                if (num % acum == 0) {
+                    break;
+                }
+                if (acum == (num / 2)) {
+                    System.out.print(num + " ");
+                    break;
+                }
+                acum++;
+            }
+            num++;
+        }
+        System.out.printf("\n---------------------------------------------------");
     }
 }
