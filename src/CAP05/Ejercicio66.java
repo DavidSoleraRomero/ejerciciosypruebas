0de8f7be92d80e9b485e8bd914f7ae1994934ee3
“DavidSoleraRomero” <soleraromerodavid@gmail.com>
2023-10-19 23:12:46 +0200

    CAP05 Ej66

--- src/CAP05/Ejercicio66.java
+++ src/CAP05/Ejercicio66.java
@@ -10,6 +10,31 @@ mensaje de error. */
 public class Ejercicio66 {
     public static void main(String[] args) {
         System.out.print("Introduce la altura de la figura: ");
-        int altura = Integer.parseInt(System.console().readLine());
+        try {
+            int altura = Integer.parseInt(System.console().readLine());
+            if (altura >= 3 & altura % 2 != 0) {
+                int pinta1 = 1;
+                int pinta2 = 6;
+                for (int i = 1; i <= altura; i++) {
+                    for (int espaciosAste = 1; espaciosAste <= altura + 5; espaciosAste++) {
+                        if (espaciosAste == pinta1 | espaciosAste == pinta2)
+                            System.out.print("*");
+                        else
+                            System.out.print(" ");
+                    }
+                    System.out.println();
+                    if (i <= altura / 2) {
+                        pinta1++;
+                        pinta2++;
+                    } else {
+                        pinta1--;
+                        pinta2--;
+                    }
+                }
+            } else
+                System.out.print("Introduce una altura impar mayor o igual a 3");
+        } catch (Exception e) {
+            System.out.println("Se ha producido un error.");
+        }
     }
 }
---
+++ src/CAP05/Ejercicio67.java
@@ -0,0 +1,10 @@
+package CAP05;
+
+/* Realiza un programa que pinte una escalera que va descendiendo de izquierda
+a derecha. El programa pedirá el número de escalones y la altura de cada
+escalón. La anchura de los escalones siempre es la misma: 4 asteriscos. */
+public class Ejercicio67 {
+    public static void main(String[] args) {
+
+    }
+}
