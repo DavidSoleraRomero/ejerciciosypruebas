a503e8093d0d4dd6a41a9e1bebbfac91e0bc4e77
“DavidSoleraRomero” <soleraromerodavid@gmail.com>
2023-10-22 20:15:10 +0200

    CAP06 Ej15

--- src/CAP06/Ejercicio15.java
+++ src/CAP06/Ejercicio15.java
@@ -14,6 +14,47 @@ con dos barras.
 d) La última nota de la melodía debe coincidir con la primera. */
 public class Ejercicio15 {
     public static void main(String[] args) {
-
+        int largo = 0;
+        do {
+            largo = (int) (Math.random() * 25 + 4);
+        } while (largo % 4 != 0);
+        String primUltim = "";
+        for (int i = 1; i <= largo; i++) {
+            int notas = (int) (Math.random() * 7 + 1);
+            String melodia = "";
+            switch (notas) {
+                case 1:
+                    melodia = "do";
+                    break;
+                case 2:
+                    melodia = "re";
+                    break;
+                case 3:
+                    melodia = "mi";
+                    break;
+                case 4:
+                    melodia = "fa";
+                    break;
+                case 5:
+                    melodia = "sol";
+                    break;
+                case 6:
+                    melodia = "la";
+                    break;
+                case 7:
+                    melodia = "si";
+                    break;
+            }
+            if (i == 1) {
+                primUltim = melodia;
+                System.out.print(primUltim + " ");
+            } else if (i == largo)
+                System.out.print(primUltim + " ");
+            else
+                System.out.print(melodia + " ");
+            if (i % 4 == 0 & i != largo)
+                System.out.print("| ");
+        }
+        System.out.println("||");
     }
 }
