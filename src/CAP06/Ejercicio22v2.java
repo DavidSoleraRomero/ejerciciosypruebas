c57268f9def8cc91e107997eeee6b74d43d5a117
DavidSoleraRomero <soleraromerodavid@gmail.com>
2023-11-15 20:24:35 +0100

    Repaso serpiente aleatoria

--- src/CAP06/Ejercicio22v2.java
+++ src/CAP06/Ejercicio22v2.java
@@ -10,12 +10,24 @@ posición a la derecha del anterior. La longitud de la serpiente se pedirá por
 teclado y se supone que el usuario introducirá un dato correcto. */
 public class Ejercicio22v2 {
     public static void main(String[] args) {
-        System.out.print("Introduce la longitud de la serpiente: ");
-        int longitud = Integer.parseInt(System.console().readLine());
-        for (int i = 1; i <= 12; i++) {
-            System.out.print(" ");
+        try {
+            System.out.print("Introduce la longitud de la serpiente: ");
+            int longitud = Integer.parseInt(System.console().readLine());
+            for (int i = 1; i <= 12; i++) {
+                System.out.print(" ");
+            }
+            System.out.println("@");
+            int random = (int) (Math.random() * 3 + 11);
+            for (int i = 1; i <= longitud - 1; i++) {
+                int anterior = random;
+                random = (int) (Math.random() * 3 + (anterior - 1));
+                for (int j = 1; j <= anterior; j++) {
+                    System.out.print(" ");
+                }
+                System.out.print("*\n");
+            }
+        } catch (Exception e) {
+            System.out.println("Error inesperado.");
         }
-        System.out.println("*");
-
     }
 }
