aa4fd16c326ae034db795cc5878d0d94d55121d9
DavidSoleraRomero <soleraromerodavid@gmail.com>
2023-12-07 22:56:55 +0100

    Fin ejercicios Arrays

--- src/CAP07/BIDIMENSIONAL/Ejercicio12.java
+++ src/CAP07/BIDIMENSIONAL/Ejercicio12.java
@@ -7,6 +7,30 @@ inferior izquierda a la esquina superior derecha, así como el máximo, el míni
 y la media de los números que hay en esa diagonal. */
 public class Ejercicio12 {
     public static void main(String[] args) {
-
+        int[][] numeros = new int[9][9];
+        System.out.println("Array original: ");
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                numeros[i][j] = (int) (Math.random() * 401 + 500);
+                System.out.print(numeros[i][j] + " ");
+            }
+            System.out.println();
+        }
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        int media = 0;
+        int fila = 8;
+        int[] diagonal = new int[9];
+        for (int i = 0; i < diagonal.length; i++) {
+            diagonal[i] = numeros[fila][i];
+            if (diagonal[i] < min)
+                min = diagonal[i];
+            if (diagonal[i] > max)
+                max = diagonal[i];
+            media = media + diagonal[i];
+            fila--;
+        }
+        System.out.println("\nLos números diagonales dan estos resultados:");
+        System.out.printf("\nLa media da %.2f\nEl máximo es %d\nEl mínimo es %d\n\n", (float) media / 9, max, min);
     }
 }
--- src/CAP07/BIDIMENSIONAL/Ejercicio13.java
+++ src/CAP07/BIDIMENSIONAL/Ejercicio13.java
@@ -10,6 +10,22 @@ los países se deben mostrar utilizando el array de países (no se pueden escrib
 directamente). */
 public class Ejercicio13 {
     public static void main(String[] args) {
-
+        String[] pais = { "España", "Rusia", "Japón", "Australia" };
+        int[][] estaturas = new int[4][10];
+        for (int i = 0; i < 4; i++) {
+            int media = 0;
+            int min = Integer.MAX_VALUE;
+            int max = Integer.MIN_VALUE;
+            for (int j = 0; j < 10; j++) {
+                estaturas[i][j] = (int) (Math.random() * 71 + 140);
+                if (estaturas[i][j] < min)
+                    min = estaturas[i][j];
+                if (estaturas[i][j] > max)
+                    max = estaturas[i][j];
+                media = media + estaturas[i][j];
+            }
+            System.out.printf("%-9s --> | Estatura media: %d | Altura máxima: %d | Altura mínima: %d |\n",
+                    pais[i], media / 10, max, min);
+        }
     }
 }
