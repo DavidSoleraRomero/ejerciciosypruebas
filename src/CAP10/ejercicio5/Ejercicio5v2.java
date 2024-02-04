319720ebb516b9e74644213b4eff1461e8bc6749
DavidSoleraRomero <soleraromerodavid@gmail.com>
2024-02-04 15:48:10 +0100

    Preparación ejercicio 5

---
+++ src/CAP10/ejercicio5/Ejercicio5v2.java
@@ -0,0 +1,10 @@
+package CAP10.ejercicio5;
+
+/* Realiza de nuevo el ejercicio de la colección de coleccion pero utilizando esta vez
+una lista para almacenar la información sobre los coleccion en lugar de un array
+convencional. Comprobarás que el código se simplifica notablemente ¿Cuánto
+ocupa el programa original hecho con un array? ¿Cuánto ocupa este nuevo
+programa hecho con una lista? */
+public class Ejercicio5v2 {
+
+}
