c43b33be7f203053372713495f227ac7fb37e5fe
DavidSoleraRomero <soleraromerodavid@gmail.com>
2024-02-06 08:27:31 +0100

    discos

--- src/CAP10/ejercicio5/ColeccionDeDiscos.java
+++ src/CAP10/ejercicio5/ColeccionDeDiscos.java
@@ -1,5 +1,9 @@
 package CAP10.ejercicio5;
 
+import java.util.HashMap;
+
 public class ColeccionDeDiscos {
-    
+
+    private HashMap<String, HashMap<String, Disco>> discos = new HashMap<>();
+
 }
