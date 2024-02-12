9ac662155790a74cc89b43798a1d540c80a0c65d
DavidSoleraRomero <soleraromerodavid@gmail.com>
2024-02-12 10:16:22 +0100

    Aprendizaje de JDBC Driver

--- src/App.java
+++ src/App.java
@@ -1,41 +1,20 @@
 /* Programa probando múltiples cosas */
 
-import generics.MyArrayList;
-import generics.Perro;
+import java.sql.Connection;
+import java.sql.DriverManager;
 
 public class App {
     public static void main(String[] args) {
-        /*
-         * MyArrayList<Integer> array = new MyArrayList<>(10);
-         * for (int i = 0; i < 1000; i++) {
-         * array.add(i);
-         * }
-         * System.out.println(array);
-         * for (int i = 0; i < 1000; i++) {
-         * array.remove(0);
-         * }
-         * System.out.println(array);
-         * for (int i = 0; i < 1000; i++) {
-         * array.add(i);
-         * }
-         * System.out.println(array);
-         * array.clear();
-         * System.out.println(array);
-         * array.add(100);
-         * array.add(120);
-         * array.add(140);
-         * System.out.println(array);
-         * array.set(1, 130);
-         * System.out.println(array);
-         */
-        MyArrayList<Perro> arrayPerros = new MyArrayList<>(100);
-        for (int i = 0; i < 1000; i++) {
-            String nombre = "";
-            for (int j = 0; j < 10; j++) {
-                nombre += "" + (char) (65 + (int) (Math.random() * 20));
-            }
-            arrayPerros.add(new Perro(nombre));
+        String url = "jdbc:mysql://localhost:3306/alumnos";
+        String usuario = "david";
+        String clave = "davidsolera";
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection _conn = DriverManager.getConnection(url, usuario, clave);
+            _conn.close();
+        } catch (Exception e) {
+            System.out.println("Error en la conexión con la BBDD");
+            e.printStackTrace();
         }
-        System.out.println(arrayPerros);
     }
 }
